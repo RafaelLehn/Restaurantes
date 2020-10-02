44c704e3e600e4264012460647889a61b1c8f1ca
Rafael Batista Silva <rafaellehn0@hotmail.com>
2020-10-01 21:23:28 -0300

    Initial Commit

---
+++ Restaurantes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Restaurantes
+//
+//  Created by Rafael Lehn Batista (P) on 01/10/20.
+//  Copyright © 2020 Rafael Lehn Batista (P). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
